5de8c5105befaf120a60542ae480be45ae33b847
TheNerdAlly <amberk@engineer.com>
2019-02-06 21:39:33 -0600

    Add files via upload

--- codeSnips/PC_OSDrive.ps1
+++ codeSnips/PC_OSDrive.ps1
@@ -9,7 +9,7 @@ function PC_OSDrive {
             #Usage:     PC_OSDrive -FIND Letter     (In many cases this would return "C")
 
             #Queries WMI to get the Logical Disk (Such as C:) and RegEX it to 1 character long (C)
-            $result = (Get-WmiObject -Class Win32_logicaldisk | Select-Object -expand DeviceID).SubString(0,1)
+            $result = ((Get-WmiObject Win32_OperatingSystem).SystemDrive).SubString(0,1)
 
             return $result
         }
