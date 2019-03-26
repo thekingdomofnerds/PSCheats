634bdb0efe2e7798509dcdd6c03a5e6108c969e2
TheNerdAlly <amberk@engineer.com>
2019-03-26 16:11:04 -0500

    Create PressAnyKeyToContinue.ps1

---
+++ codeSnips/Functions/PressAnyKeyToContinue.ps1
@@ -0,0 +1,15 @@
+#Should work in PSVersions 5 in Powershell, ISE, and VSCode
+Function pause ($message)
+{
+    # Check if running Powershell ISE
+    if ($psISE)
+    {
+        Add-Type -AssemblyName System.Windows.Forms
+        [System.Windows.Forms.MessageBox]::Show("$message")
+    }
+    else
+    {
+        Write-Host "$message" -ForegroundColor Yellow
+        $x = $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    }
+}
